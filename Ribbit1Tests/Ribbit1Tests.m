5c5e6246bfde61469453721a82a150d1c8aeffef
Kerry Toonen <kerrytoonen@gmail.com>
2016-05-19 19:52:47 -0400

    Initial Commit

---
+++ Ribbit1Tests/Ribbit1Tests.m
@@ -0,0 +1,39 @@
+//
+//  Ribbit1Tests.m
+//  Ribbit1Tests
+//
+//  Created by Kerry Toonen on 2016-05-19.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ribbit1Tests : XCTestCase
+
+@end
+
+@implementation Ribbit1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
